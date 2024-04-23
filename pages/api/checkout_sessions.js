dac6398c85a224a7e4a16a805c9d42fb4c4c75b0
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-04-23 17:35:46 +0200

    change of keys

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET);
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 export default async function handler(req, res) {
     if (req.method === "POST") {
