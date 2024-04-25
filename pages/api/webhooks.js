22ce07eac06b4869b0611bb0dd32bfb80890b559
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-04-25 15:17:20 +0200

    webhook

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
 
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET);
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
