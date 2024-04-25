484152d32836a55693342d90ee6698eab1b43ad1
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-04-25 14:49:07 +0200

    webhook

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -1,9 +1,14 @@
 import Stripe from "stripe";
+import { createClient } from "@supabase/supabase-js";
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const config = {
     api: {
-        bodyParser: false, // Disabling body parsing
+        bodyParser: false,
     },
 };
 
@@ -26,8 +31,46 @@ export default async function handler(req, res) {
         }
 
         if (event.type === "checkout.session.completed") {
-            console.log("Payment was successful!");
-            // Perform actions based on the successful payment
+            console.log("Payment was successful! it really was!");
+
+            // Retrieve the session to get line items
+            const sessionId = event.data.object.id;
+            const session = await stripe.checkout.sessions.retrieve(sessionId, {
+                expand: ["line_items.data"],
+            });
+
+            // Accumulate quantities for each item
+            const itemCounts = session.line_items.data.reduce((acc, item) => {
+                acc[item.description] =
+                    (acc[item.description] || 0) + item.quantity;
+                return acc;
+            }, {});
+
+            // Update inventory in Supabase for each unique item
+            for (const [sizeStockId, quantity] of Object.entries(itemCounts)) {
+                const { data, error } = await supabase
+                    .from("sizesStock")
+                    .update({
+                        amount: supabase.functions.raw("amount - ?", [
+                            quantity,
+                        ]),
+                    })
+                    .eq("id", sizeStockId);
+
+                if (error) {
+                    console.error(
+                        "Error updating inventory for item:",
+                        sizeStockId,
+                        error.message
+                    );
+                } else {
+                    console.log(
+                        "Inventory updated for item:",
+                        sizeStockId,
+                        data
+                    );
+                }
+            }
         }
 
         res.json({ received: true });
