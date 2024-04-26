cf5356a7bd75865cac4ea84afdee16959e2a2cba
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-04-26 17:02:19 +0200

    checkchehcechanges

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -12,17 +12,27 @@ export default async function handler(req, res) {
                     .json({ error: "Invalid request format" });
             }
 
+            // Serialize the cart items to store in metadata
+            const cartMetadata = JSON.stringify(
+                items.map((item) => ({
+                    stripe_id: item.price, // Assuming `price` here actually refers to stripe_id in your model
+                    quantity: item.quantity,
+                    sizeStockId: item.sizeStockId, // Ensure sizeStockId is passed from the client
+                }))
+            );
+
             const session = await stripe.checkout.sessions.create({
                 line_items: items,
                 mode: "payment",
                 success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
                 cancel_url: `${req.headers.origin}/cancel`,
+                metadata: { cart: cartMetadata }, // Attach serialized cart data
                 automatic_tax: { enabled: true },
                 shipping_address_collection: {
-                    allowed_countries: ["NO"], // Specify allowed countries here
+                    allowed_countries: ["NO"],
                 },
                 shipping_options: [
-                    { shipping_rate: "shr_1P06OnGTrv1TY0preYT0wO14" }, // Example shipping rate ID
+                    { shipping_rate: "shr_1P06OnGTrv1TY0preYT0wO14" },
                 ],
             });
             res.status(200).json({ sessionId: session.id });
