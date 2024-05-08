1edc8a28205e85967a19f5b0668c67c050199ae2
Eskil K. Hagen <eskilkaspersen@gmail.com>
2024-05-08 16:35:58 +0200

    remove shipping

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -34,9 +34,6 @@ export default async function handler(req, res) {
                 shipping_address_collection: {
                     allowed_countries: ["NO"],
                 },
-                shipping_options: [
-                    { shipping_rate: "shr_1PDokZFsFXjrirmTCiYCZaFp" },
-                ],
             });
 
             // Successfully created session
